c2103556b2a50002caa8b900ce82ac3703555bd8
jquintos <jquintos@localhost.localdomain>
2017-10-30 02:20:43 -0700

    Test

---
+++ src/pages/browse/browse.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { UpcomingPage } from './upcoming/upcoming';
+/**
+ * Generated class for the BrowsePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-browse',
+  templateUrl: 'browse.html',
+})
+export class BrowsePage {
+  tab1Root = UpcomingPage;
+  tab2Root = UpcomingPage;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+
+}
---
+++ src/pages/browse/upcoming/upcoming.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-upcoming',
+  templateUrl: 'upcoming.html'
+})
+export class UpcomingPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
